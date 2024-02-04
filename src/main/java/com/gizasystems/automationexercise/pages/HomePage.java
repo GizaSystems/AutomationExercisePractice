404d733951c336552c191f6ea624110edf5aab96
MustafaAgamy <mostafaagamy1995@gmail.com>
2024-02-04 02:56:27 +0200

    Add Methods related to TC22 To HomPage

--- src/main/java/com/gizasystems/automationexercise/pages/HomePage.java
+++ src/main/java/com/gizasystems/automationexercise/pages/HomePage.java
@@ -12,7 +12,8 @@ public class HomePage {
     // Locators
     private final By featuredItems_div = By.cssSelector("div.features_items");
     private final By recommendedItems_div = By.cssSelector("div.recommended_items");
-
+    private final By viewCartBtn = By.xpath("//div[contains(@class,'confirm')]//a[@href='/view_cart']");
+    private final By productAddedToCartMessage = By.cssSelector("div.modal-content > div > h4");
     // Constructor
     public HomePage(SHAFT.GUI.WebDriver driver) {
         this.driver = driver;
@@ -24,12 +25,39 @@ public class HomePage {
         driver.browser().navigateToURL(url);
         return this;
     }
+    @Step("Navigate Recommended Section Tab")
+    public HomePage openRecommendedSection(){
+        driver.element().hover(recommendedItems_div);
+        isRecommendedSectionVisible();
+        return this;
+    }
+    @Step("Add Recommended Product To Cart")
+    public HomePage addToCart(String productName){
+        String productNameValue = "//div[@class='recommended_items']//child::p[text()='" + productName + "']//parent::div//a";
+        driver.element().click(By.xpath(productNameValue));
+        return this;
+    }
+    @Step("Open Cart Page")
+    public CartPage openCart(){
+        driver.element().click(viewCartBtn);
+        return new CartPage(driver);
+    }
 
     //////////////////// Validations \\\\\\\\\\\\\\\\\\\\
     @Step("Validate on Visibility of the Home Page")
     public HomePage validateOnVisibilityOfHomePage() {
         driver.verifyThat().element(featuredItems_div).exists().perform();
         driver.verifyThat().element(recommendedItems_div).exists().perform();
+        return this;
+    }
+    @Step("Validate on Visibility of Recommended Section")
+    public HomePage isRecommendedSectionVisible(){
+        driver.verifyThat().element(recommendedItems_div).isVisible().perform();
+        return this;
+    }
+    @Step("Validate on Visibility of Successful Add To Cart Message")
+    public HomePage isProductAddedSuccessfullyToCart(String message){
+        driver.verifyThat().element(productAddedToCartMessage).textTrimmed().isEqualTo(message).perform();
         return this;
     }
 
