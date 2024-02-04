1ef1f52a42452ff89608616e94789d18b64a8254
MustafaAgamy <mostafaagamy1995@gmail.com>
2024-02-04 13:38:07 +0200

    move methods and variables from HomePage to Recommended Section class

--- src/main/java/com/gizasystems/automationexercise/pages/HomePage.java
+++ src/main/java/com/gizasystems/automationexercise/pages/HomePage.java
@@ -15,8 +15,6 @@ public class HomePage {
     // Locators
     private final By featuredItems_div = By.cssSelector("div.features_items");
     private final By recommendedItems_div = By.cssSelector("div.recommended_items");
-    private final By viewCartBtn = By.xpath("//div[contains(@class,'confirm')]//a[@href='/view_cart']");
-    private final By productAddedToCartMessage = By.cssSelector("div.modal-content > div > h4");
     // Constructor
     public HomePage(SHAFT.GUI.WebDriver driver) {
         this.driver = driver;
@@ -28,25 +26,15 @@ public class HomePage {
         driver.browser().navigateToURL(url);
         return this;
     }
+
     @Step("Navigate Recommended Section Tab")
-    public HomePage openRecommendedSection(){
+    public RecommendedSection openRecommendedSection(){
         JavascriptExecutor javascriptExecutor = (JavascriptExecutor) driver.getDriver();
         WebElement webElement = driver.getDriver().findElement(recommendedItems_div);
         javascriptExecutor.executeScript("arguments[0].scrollIntoView({behavior: 'smooth', block: 'center', inline: 'center'})",webElement);
         Actions actions = new Actions(driver.getDriver());
         actions.pause(2000).moveToElement(webElement).perform();
-        return this;
-    }
-    @Step("Add Recommended Product To Cart")
-    public HomePage addToCart(String productName){
-        String productNameValue = "//div[@class='recommended_items']//child::p[text()='" + productName + "']//parent::div//a";
-        driver.element().click(By.xpath(productNameValue));
-        return this;
-    }
-    @Step("Open Cart Page")
-    public CartPage openCart(){
-        driver.element().click(viewCartBtn);
-        return new CartPage(driver);
+        return new RecommendedSection(driver);
     }
 
     //////////////////// Validations \\\\\\\\\\\\\\\\\\\\
@@ -56,15 +44,6 @@ public class HomePage {
         driver.verifyThat().element(recommendedItems_div).exists().perform();
         return this;
     }
-    @Step("Validate on Visibility of Recommended Section")
-    public HomePage isRecommendedSectionVisible(){
-        driver.verifyThat().element(recommendedItems_div).isVisible().perform();
-        return this;
-    }
-    @Step("Validate on Visibility of Successful Add To Cart Message")
-    public HomePage isProductAddedSuccessfullyToCart(String message){
-        driver.verifyThat().element(productAddedToCartMessage).textTrimmed().isEqualTo(message).perform();
-        return this;
-    }
+
 
 }
