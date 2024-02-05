6e6a985380fb1ef10e10f93736c0a2c2d7e7ba05
AlaaMahmoudFawzy <alaa.fawzy@gizasystems.com>
2024-02-05 09:04:05 +0200

    Adding Cart Page

---
+++ src/main/java/com/gizasystems/automationexercise/pages/CartPage.java
@@ -0,0 +1,53 @@
+package com.gizasystems.automationexercise.pages;
+
+import com.shaft.driver.SHAFT;
+import io.qameta.allure.Step;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+
+public class CartPage {
+    private SHAFT.GUI.WebDriver driver;
+
+    private String url = System.getProperty("baseUrl") + "/view_cart";
+
+    By subscriptionTxt = By.tagName("h2");
+
+    private final By subscription_input = By.id("susbscribe_email");
+    private final By subscribeBtn = By.id("subscribe");
+
+    private final By successAlert = By.xpath("//div[@class='alert-success alert']");
+
+    public CartPage(SHAFT.GUI.WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public CartPage navigate() {
+        driver.browser().navigateToURL(url);
+        return this;
+    }
+
+    @Step("Validate on Visibility of the subscription Text")
+    public CartPage validateOnVisibilityOfSubscriptionText() {
+        driver.verifyThat().element(subscriptionTxt).text().isEqualTo("Subscription");
+        return this;
+    }
+
+   @Step("Enter Subscription Email")
+    public CartPage enterSubscriptionEmail(String email) {
+        driver.element().type(subscription_input,email);
+        return this;
+    }
+
+    @Step("Click on Subscribe Button")
+    public CartPage clickOnSubscribeButton() {
+        driver.element().click(subscribeBtn);
+        return this;
+    }
+
+    @Step("Validate on Success Message of Subscription Email")
+    public CartPage ValidateOnSuccessMessageOfSubscriptionEmail(String expectedText) {
+        driver.verifyThat().element(successAlert).text().isEqualTo(expectedText);
+        return this;
+    }
+
+}
