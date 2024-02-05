81e299e44bb3d5a000bd62b41da7fdf0db14871a
Mariam Elsayed Abduallah <mariam.abduallah@gizasystems.local>
2024-02-05 11:57:04 +0200

    Add login form locators and login functions

--- src/main/java/com/gizasystems/automationexercise/pages/SignupLoginPage.java
+++ src/main/java/com/gizasystems/automationexercise/pages/SignupLoginPage.java
@@ -16,7 +16,11 @@ public class SignupLoginPage {
     private final By signupUserName_input = By.xpath("//input[@data-qa='signup-name']");
     private final By signupEmail_input = By.xpath("//input[@data-qa='signup-email']");
     private final By signup_button = By.xpath("//button[@data-qa='signup-button']");
-
+    private final By login_div= By.xpath("//div[@class='login-form']");
+    private final By login_h2 = By.xpath("//div[@class='login-form']//h2");
+    private final By loginEmail_input = By.xpath("//input[@data-qa='login-email']");
+    private final By loginPassword_input= By.xpath("//input[@data-qa='login-password']");
+    private final By login_button = By.xpath("//button[@data-qa='login-button']");
     // Constructor
     public SignupLoginPage(SHAFT.GUI.WebDriver driver) {
         this.driver = driver;
@@ -35,12 +39,26 @@ public class SignupLoginPage {
                 .click(signup_button);
         return this;
     }
+    @Step("User Login with Correct Email: {email} and Password: {password}")
+    public SignupLoginPage registeredUserLogin(String email, String password){
+        driver.element()
+                .type(loginEmail_input, email)
+                .type(loginPassword_input, password)
+                .click(login_button);
+        return this;
+    }
 
     //////////////////// Validations \\\\\\\\\\\\\\\\\\\\
     @Step("Validate on Signup Visibility")
     public SignupLoginPage validateOnSignUpVisibility(String expectedResult) {
         driver.verifyThat().element(signup_div).exists().perform();
         driver.verifyThat().element(signup_h2).text().isEqualTo(expectedResult).perform();
+        return this;
+    }
+    @Step("Validate on Login Visibility")
+    public SignupLoginPage validateOnLoginVisibility(String expectedResult){
+        driver.verifyThat().element(login_div).exists().perform();
+        driver.verifyThat().element(login_h2).text().isEqualTo(expectedResult).perform();
         return this;
     }
 
