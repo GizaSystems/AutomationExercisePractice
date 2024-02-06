267624acce9db56c30cf4388104f261ded5fb90e
AlaaMahmoudFawzy <alaa.fawzy@gizasystems.com>
2024-02-06 06:46:53 +0200

    Adding perform

--- src/main/java/com/gizasystems/automationexercise/pages/CartPage.java
+++ src/main/java/com/gizasystems/automationexercise/pages/CartPage.java
@@ -3,7 +3,6 @@ package com.gizasystems.automationexercise.pages;
 import com.shaft.driver.SHAFT;
 import io.qameta.allure.Step;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 
 public class CartPage {
     private SHAFT.GUI.WebDriver driver;
@@ -25,28 +24,24 @@ public class CartPage {
         driver.browser().navigateToURL(url);
         return this;
     }
-
-    @Step("Validate on Visibility of the subscription Text")
-    public CartPage validateOnVisibilityOfSubscriptionText() {
-        driver.verifyThat().element(subscriptionTxt).text().isEqualTo("Subscription");
-        return this;
-    }
-
    @Step("Enter Subscription Email")
     public CartPage enterSubscriptionEmail(String email) {
         driver.element().type(subscription_input,email);
         return this;
     }
-
     @Step("Click on Subscribe Button")
     public CartPage clickOnSubscribeButton() {
         driver.element().click(subscribeBtn);
         return this;
     }
-
+    @Step("Validate on Visibility of the subscription Text")
+    public CartPage validateOnVisibilityOfSubscriptionText() {
+        driver.verifyThat().element(subscriptionTxt).text().isEqualTo("SUBSCRIPTION").perform();
+        return this;
+    }
     @Step("Validate on Success Message of Subscription Email")
     public CartPage ValidateOnSuccessMessageOfSubscriptionEmail(String expectedText) {
-        driver.verifyThat().element(successAlert).text().isEqualTo(expectedText);
+        driver.verifyThat().element(successAlert).text().isEqualTo(expectedText).perform();
         return this;
     }
 
