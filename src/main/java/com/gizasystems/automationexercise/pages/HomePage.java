f43bad2de7d54816270ab71ca6cd9b32d1646435
hadeer.alaa <hadeer.alaa@gizasystems.com>
2024-02-08 17:54:16 +0300

    adding lines empty

--- src/main/java/com/gizasystems/automationexercise/pages/HomePage.java
+++ src/main/java/com/gizasystems/automationexercise/pages/HomePage.java
@@ -1,30 +1,27 @@
 package com.gizasystems.automationexercise.pages;
-
 import com.shaft.driver.SHAFT;
 import io.qameta.allure.Step;
 import org.openqa.selenium.By;
-
 public class HomePage {
     // Variables
     private SHAFT.GUI.WebDriver driver;
     private String url = System.getProperty("baseUrl");
-
     // Locators
     private final By featuredItems_div = By.cssSelector("div.features_items");
     private final By recommendedItems_div = By.cssSelector("div.recommended_items");
-
+    public By getCategoryTitleLocator(String categoryTitle ){
+        return By.xpath("//h2[text()='"+ categoryTitle +"']");
+    }
     // Constructor
     public HomePage(SHAFT.GUI.WebDriver driver) {
         this.driver = driver;
     }
-
     //////////////////// Actions \\\\\\\\\\\\\\\\\\\\
     @Step("Navigate to Home Page")
     public HomePage navigate() {
         driver.browser().navigateToURL(url);
         return this;
     }
-
     //////////////////// Validations \\\\\\\\\\\\\\\\\\\\
     @Step("Validate on Visibility of the Home Page")
     public HomePage validateOnVisibilityOfHomePage() {
@@ -32,5 +29,9 @@ public class HomePage {
         driver.verifyThat().element(recommendedItems_div).exists().perform();
         return this;
     }
-
+    @Step("Validate on Visibility of the Category Title")
+    public HomePage validateOnVisibilityOfCategoryTitle(String categoryTitle) {
+        driver.verifyThat().element(getCategoryTitleLocator(categoryTitle)).exists().perform();
+        return this;
+    }
 }
