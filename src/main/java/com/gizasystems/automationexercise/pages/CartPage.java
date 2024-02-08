3656d916d009cba6ef504f75f8140a8c0d9294d2
marina.gohar <marina.gohar@GizaSystems.onmicrosoft.com>
2024-02-08 12:16:49 +0200

    added an enhancement to the Cart Page

--- src/main/java/com/gizasystems/automationexercise/pages/CartPage.java
+++ src/main/java/com/gizasystems/automationexercise/pages/CartPage.java
@@ -15,21 +15,21 @@ public class CartPage {
 
     private String url = System.getProperty("baseUrl") + "/view_cart";
 
-    private final By firstProductName = By.cssSelector("a[href='/product_details/1']");
+    private final By firstProductName = By.xpath("//a[normalize-space()='Blue Top']");
 
-    private final By secondProductName = By.cssSelector("a[href='/product_details/2']");
+    private final By secondProductName = By.xpath("//a[normalize-space()='Men Tshirt']");
 
-    private final By firstProductPrice = By.xpath("(//td[@class='cart_price']//p)[1]");
+    private final By firstProductPrice = By.xpath("//td[@class='cart_price']//p[contains(text(),'Rs. 500')]");
 
-    private final By secondProductPrice = By.xpath("(//td[@class='cart_price']//p)[2]");
+    private final By secondProductPrice = By.xpath("//td[@class='cart_price']//p[contains(text(),'Rs. 400')]");
 
-    private final By firstProductQuantity = By.xpath("(//button[@class='disabled'])[1]");
+    private final By firstProductQuantity = By.xpath("//tr[@id='product-1']//button[@class='disabled'][normalize-space()='1']");
 
-    private final By secondProductQuantity = By.xpath("(//button[@class='disabled'])[2]");
+    private final By secondProductQuantity = By.xpath("//tr[@id='product-2']//button[@class='disabled'][normalize-space()='1']");
 
-    private final By firstProductTotal = By.xpath("(//p[@class=\"cart_total_price\"])[1]");
+    private final By firstProductTotal = By.xpath("//p[@class='cart_total_price'][normalize-space()='Rs. 500']");
 
-    private final By secondProductTotal = By.xpath("(//p[@class=\"cart_total_price\"])[2]");
+    private final By secondProductTotal = By.xpath("//p[@class='cart_total_price'][normalize-space()='Rs. 400']");
 
     // Constructor
     public CartPage(SHAFT.GUI.WebDriver driver){
