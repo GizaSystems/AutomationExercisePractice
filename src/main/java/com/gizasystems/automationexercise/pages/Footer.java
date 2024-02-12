322fcafe0ce38e946d8396c9ba2dff3394e84488
basant.lazem <basant.lazem@gizasystems.onmicrosoft.com>
2024-02-12 14:27:13 +0200

    Euqals Igonre

--- src/main/java/com/gizasystems/automationexercise/pages/Footer.java
+++ src/main/java/com/gizasystems/automationexercise/pages/Footer.java
@@ -6,8 +6,6 @@ import org.openqa.selenium.By;
 
 public class Footer {
     private SHAFT.GUI.WebDriver driver;
-    private final By recommendedItems_div = By.cssSelector("div.recommended_items");
-
     private final By subscriptionText = By.xpath("//div[@class='single-widget']/h2");
     private final By subscriptionEmail = By.id("susbscribe_email");
     private final By subscribeButton = By.id("subscribe");
@@ -18,7 +16,7 @@ public class Footer {
     }
     @Step("Verify Subscription Text is Visible")
     public Footer  VerifySubscriptionText(String Text) {
-        driver.verifyThat().element(subscriptionText).text().isEqualTo(Text).perform();
+        driver.verifyThat().element(subscriptionText).text().equalsIgnoringCaseSensitivity(Text).perform();
         return this;
     }
     @Step("Enter Subscription Email")
