c59b11d4b6551fd4f9bea279e40268e0a3a9870c
Abdelaziz Ali <57860843+abdelazizali678@users.noreply.github.com>
2024-02-21 21:36:54 +0200

    LogoutUser Resolved comments

--- src/main/java/com/gizasystems/automationexercise/pages/LogoutUser.java
+++ src/main/java/com/gizasystems/automationexercise/pages/LogoutUser.java
@@ -2,31 +2,22 @@ package com.gizasystems.automationexercise.pages;
 
 import com.shaft.driver.SHAFT;
 import io.qameta.allure.Step;
-import org.apache.xmlbeans.impl.xb.xsdschema.Public;
 import org.openqa.selenium.By;
 
-public class LogoutUser{
+public class LogoutUser {
 
     //Variables
     private SHAFT.GUI.WebDriver driver;
     private final String url = System.getProperty("baseUrl");
 
-
-
     //Locators
 
-
-    private final By Logout_button = By.xpath("//header//ul//li[4]/a");
-
-
-
+    private final By Logout_button = By.xpath("a[href='/logout']");
 
     //Constructors
-    public LogoutUser(SHAFT.GUI.WebDriver driver){
-        this.driver = driver ;
+    public LogoutUser(SHAFT.GUI.WebDriver driver) {
+        this.driver = driver;
     }
-
-
 
     //////////////////////////Actions\\\\\\\\\\\\\\\\\\\\
     @Step("Navigate to Home Page")
@@ -34,12 +25,5 @@ public class LogoutUser{
         driver.element().click(Logout_button);
         return this;
     }
-
-
-
-    ///////////////////////Validations\\\\\\\\\\\\\\\\\\\\
-
-
-
 
 }
--- src/test/java/com/gizasystems/automationexercise/tests/logoutUser/logOutUser.java
+++ src/test/java/com/gizasystems/automationexercise/tests/logoutUser/logOutUser.java
@@ -1,11 +1,10 @@
 package com.gizasystems.automationexercise.tests.logoutUser;
 
 import com.gizasystems.automationexercise.pages.HomePage;
+import com.gizasystems.automationexercise.pages.LogoutUser;
 import com.gizasystems.automationexercise.pages.NavigationBar;
 import com.gizasystems.automationexercise.pages.SignupLoginPage;
-import com.gizasystems.automationexercise.pages.SignupPage;
 import com.shaft.driver.SHAFT;
-import groovy.lang.GString;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Story;
@@ -14,28 +13,20 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-
 @Epic("Automation Exercise")
 @Feature("User Management")
 @Story("Logout User")
 
 public class logOutUser {
 
-
     private SHAFT.GUI.WebDriver driver;
-
     private SHAFT.TestData.JSON testData;
 
-    private String url = System.getProperty("url");
     @Test
-    public void SuccessfulLogOut(){
-
-
-
-        new com.gizasystems.automationexercise.pages.LogoutUser(driver).logOut();
+    public void SuccessfulLogOut() {
+        new LogoutUser(driver).logOut();
         new SignupLoginPage(driver).validateOnLoginVisibility(testData.getTestData("Messages.Login"));
     }
-
 
     @BeforeClass
     public void beforeClass() {
@@ -45,11 +36,9 @@ public class logOutUser {
     @BeforeMethod
     public void beforeMethod() {
         driver = new SHAFT.GUI.WebDriver();
-        new HomePage(driver)
-                .navigate()
-                .validateOnVisibilityOfHomePage();
+        new HomePage(driver).navigate().validateOnVisibilityOfHomePage();
         new NavigationBar(driver).clickOnSignupLoginLink();
-        new SignupLoginPage(driver).registeredUserLogin("abdelazizaliabdelaziz@gmail.com" ,"karimAhmed");
+        new SignupLoginPage(driver).registeredUserLogin(testData.getTestData("UserName"), testData.getTestData("UserPassword"));
     }
 
     @AfterMethod
