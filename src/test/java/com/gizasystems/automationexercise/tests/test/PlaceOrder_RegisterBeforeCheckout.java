66b7740ec98c1d3c6d72d2a0686ccdc6631aca69
basant.lazem <basant.lazem@gizasystems.onmicrosoft.com>
2024-02-22 12:46:31 +0200

    Deleting unecessary imports

--- src/test/java/com/gizasystems/automationexercise/tests/test/PlaceOrder_RegisterBeforeCheckout.java
+++ src/test/java/com/gizasystems/automationexercise/tests/test/PlaceOrder_RegisterBeforeCheckout.java
@@ -1,15 +1,10 @@
 package com.gizasystems.automationexercise.tests.test;
+
 import com.gizasystems.automationexercise.pages.*;
-import com.shaft.driver.DriverFactory;
 import com.shaft.driver.SHAFT;
-import io.qameta.allure.Description;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Story;
-import org.openqa.selenium.By;
-import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.firefox.FirefoxProfile;
-import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
