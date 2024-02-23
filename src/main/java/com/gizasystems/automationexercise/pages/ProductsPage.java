2829ab4f624fe14d6e50a10130249da20424f7db
abdelrahmangamal1998 <abdelrahmangamal.h@gmail.com>
2024-02-23 05:59:26 +0200

    Added ProductsPage.java with search product test case implementations, SearchProductsTest.java to call test cases, and search_product_data.json for test data

---
+++ src/main/java/com/gizasystems/automationexercise/pages/ProductsPage.java
@@ -0,0 +1,48 @@
+package com.gizasystems.automationexercise.pages;
+import com.gizasystems.automationexercise.utils.GoogleAlert;
+import com.shaft.driver.SHAFT;
+import io.qameta.allure.Step;
+import org.openqa.selenium.By;
+
+
+public class ProductsPage {
+    private SHAFT.GUI.WebDriver driver;
+
+    public ProductsPage(SHAFT.GUI.WebDriver driver) {
+        this.driver = driver;
+    }
+
+    private final By searchTextArea = By.xpath("//input[@id='search_product']");
+    private final By searchedProducts = By.xpath("//h2[@class='title text-center']");
+    private final By allProducts = By.xpath("//h2[@class='title text-center']");
+    private final By searchButton = By.xpath("//button[@id='submit_search']");
+    private final By searchResult = By.xpath("//div[@class='productinfo text-center']//p");
+
+    @Step("Enter product name in search input and click search button")
+    public ProductsPage searchForProduct(String SearchedProduct) {
+        driver.element().type(searchTextArea, SearchedProduct);
+        driver.element().click(searchButton);
+        return this;
+    }
+
+    @Step("Verify user is navigated to ALL PRODUCTS page successfully")
+    public ProductsPage validateOnallProductPage(String allProductsPage) {
+        GoogleAlert.dismissAlert(driver, allProducts);
+        driver.assertThat().element(allProducts).text().isEqualTo(allProductsPage).perform();
+        return this;
+    }
+
+    @Step("Verify 'SEARCHED PRODUCTS' is visible")
+    public ProductsPage validateOnsearchedProducts(String searchedProductPage) {
+        driver.element().assertThat(searchedProducts).text().isEqualTo(searchedProductPage).perform();
+        return this;
+    }
+
+    @Step("Verify all the products related to search are visible")
+    public ProductsPage validateOnProductsRelatedToSearch(String SearchResult) {
+        driver.element().assertThat(searchResult).text().isEqualTo(SearchResult).perform();
+        return this;
+    }
+
+
+}
---
+++ src/test/java/com/gizasystems/automationexercise/tests/SearchProducts/SearchProductsTest.java
@@ -0,0 +1,51 @@
+package com.gizasystems.automationexercise.tests.SearchProducts;
+
+import com.gizasystems.automationexercise.pages.HomePage;
+import com.gizasystems.automationexercise.pages.NavigationBar;
+import com.gizasystems.automationexercise.pages.ProductsPage;
+import com.shaft.driver.SHAFT;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+@Epic("Automation Exercise")
+@Feature("Products Page")
+@Story("Search Product")
+public class SearchProductsTest {
+    private SHAFT.GUI.WebDriver driver;
+    private SHAFT.TestData.JSON testData;
+
+    @BeforeClass
+    public void beforeClass() {
+        testData = new SHAFT.TestData.JSON("src/test/resources/testDataFiles/SearchProduct.json");
+    }
+
+    @BeforeMethod
+    public void beforeMethod() {
+
+        driver = new SHAFT.GUI.WebDriver();
+        new HomePage(driver)
+                .navigate()
+                .validateOnVisibilityOfHomePage();
+    }
+
+    @Test(description = "Search Product")
+    public void searchProduct() {
+
+        new NavigationBar(driver).navigateToProductsPage();
+        new ProductsPage(driver).validateOnallProductPage(testData.getTestData("allProductsPage"))
+                .searchForProduct(testData.getTestData("SearchedProduct"))
+                .validateOnsearchedProducts(testData.getTestData("searchedProductPage"))
+                .validateOnProductsRelatedToSearch(testData.getTestData("SearchResult"));
+    }
+
+
+    @AfterMethod
+    public void afterMethod() {
+        driver.quit();
+    }
+}
