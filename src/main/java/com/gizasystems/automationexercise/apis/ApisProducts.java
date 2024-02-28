f8fb34f9d5a32f1de66cb07b0d98a31112e095cd
Mariam Elsayed Abduallah <mariam.abduallah@gizasystems.local>
2024-02-28 14:12:23 +0200

    solve method name

--- src/main/java/com/gizasystems/automationexercise/apis/ApisProducts.java
+++ src/main/java/com/gizasystems/automationexercise/apis/ApisProducts.java
@@ -4,6 +4,7 @@ import com.shaft.api.RestActions;
 import com.shaft.driver.SHAFT;
 import io.qameta.allure.Step;
 import io.restassured.http.ContentType;
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -16,8 +17,8 @@ public class ApisProducts {
 
     // Services
     private static final String getAllProductsList_serviceName = "/productsList";
-private static final String searchProduct_serviceName = "/searchProduct";
-  
+    private static final String searchProduct_serviceName = "/searchProduct";
+
     //////////////////// Actions \\\\\\\\\\\\\\\\\\\\
     @Step("API Get All Products List")
     public ApisProducts getAllProductList() {
@@ -25,7 +26,7 @@ private static final String searchProduct_serviceName = "/searchProduct";
                 .setContentType(ContentType.URLENC)
                 .setTargetStatusCode(Apis.SUCCESS)
                 .perform();
-return this;
+        return this;
     }
 
     @Step("API Search For Product")
@@ -38,20 +39,21 @@ return this;
                 .setParameters(formData, RestActions.ParametersType.FORM)
                 .setContentType(ContentType.URLENC)
                 .setTargetStatusCode(Apis.SUCCESS)
-                .perform();    
+                .perform();
         return this;
     }
 
     //////////////////// Validations \\\\\\\\\\\\\\\\\\\\
-  
+
     @Step("Validate All products are listed")
-    public ApisProducts validateAllProductsAreListed(String productName) {
+    public ApisProducts validateLastProductName(String productName) {
         api.assertThatResponse()
                 .extractedJsonValue("$.products[(@.length-1)].name")
                 .isEqualTo(productName)
                 .perform();
-  return this;
-}
+        return this;
+    }
+
     public ApisProducts validateOnCategory(String category) {
         for (int i = 0; i < 10; i++) {
             api.assertThatResponse().extractedJsonValue("products[" + i + "].category.category").contains(category)
@@ -59,6 +61,6 @@ return this;
         }
         return this;
     }
-    
+
 
 }
