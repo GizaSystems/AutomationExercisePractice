8d0489a9c07ef6efa9f16b87e30755f5e01190f6
basant.lazem <basant.lazem@gizasystems.onmicrosoft.com>
2024-03-12 16:15:47 +0200

    removing unused import

--- src/test/java/com/gizasystems/automationexercise/tests/VerifyingSubscriptionInHomePage.java
+++ src/test/java/com/gizasystems/automationexercise/tests/VerifyingSubscriptionInHomePage.java
@@ -6,7 +6,6 @@ import io.qameta.allure.*;
 import jdk.jfr.Description;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
 import org.testng.annotations.BeforeClass;
 
 @Epic("Automation Exercise")
