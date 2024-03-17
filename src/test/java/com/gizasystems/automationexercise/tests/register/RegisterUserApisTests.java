b8ed2b989f7d8f1373f1a6bc9e902af17edaac2e
mahmoud.elsharkawy <mahmoud.elsharkawy@gizasystems.com>
2024-03-17 13:09:35 +0200

    Enhancements

--- src/test/java/com/gizasystems/automationexercise/tests/register/RegisterUserApisTests.java
+++ src/test/java/com/gizasystems/automationexercise/tests/register/RegisterUserApisTests.java
@@ -8,8 +8,6 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.util.UUID;
-
 @Epic("Automation Exercise")
 @Feature("User Management")
 @Story("Register")
