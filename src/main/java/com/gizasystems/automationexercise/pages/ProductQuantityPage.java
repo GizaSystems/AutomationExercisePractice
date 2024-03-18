efc6dc1a5683e2ed6c0260e59fca5d309bc461c0
Mariam Elsayed Abduallah <mariam.abduallah@gizasystems.local>
2024-03-18 15:52:22 +0200

    Enhance ProductQuantityPage , add page refresh after cart opened

--- src/main/java/com/gizasystems/automationexercise/pages/ProductQuantityPage.java
+++ src/main/java/com/gizasystems/automationexercise/pages/ProductQuantityPage.java
@@ -4,6 +4,7 @@ import com.gizasystems.automationexercise.utils.GoogleAlert;
 import com.shaft.driver.SHAFT;
 import io.qameta.allure.Step;
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ProductQuantityPage {
     // Locators
@@ -11,9 +12,9 @@ public class ProductQuantityPage {
     private final By viewProductbtn_button = By.xpath("(//div[@class='choose']//a)[2]");
     private final By productDetails = By.xpath("//span/Label");
     private final By hoverBtn_button = By.id("quantity");
-    private final By addToCartbtn_Button = By.xpath("//button [@class= 'btn btn-default cart']");
-    private final By viewCartbtn_button = By.xpath("//a[@href='/view_cart']/u");
-    private final By productQuantity_button = By.xpath("//td[@class = 'cart_quantity']/button[@class='disabled']");
+    private final By addToCartbtn_Button = By.cssSelector("button[class='btn btn-default cart']");
+    private final By viewCartbtn_button = By.xpath("//p[@class='text-center']/a[@href='/view_cart']/u");
+    private final By productQuantity_button = By.xpath("//td[@class ='cart_quantity']/button[@class='disabled']");
 
     // Constructor
     public ProductQuantityPage(SHAFT.GUI.WebDriver driver) {
@@ -26,7 +27,11 @@ public class ProductQuantityPage {
         GoogleAlert.dismissAlert(driver, viewProductbtn_button);
         return this;
     }
-
+    @Step("Verify that Product details is opened")
+    public ProductQuantityPage verifyOnProductDetails() {
+        driver.element().assertThat(productDetails).exists().perform();
+        return this;
+    }
     @Step("Increase Quantity in Cart")
     public ProductQuantityPage increaseQuantityInCart(String quantity) {
         driver.element()
@@ -42,9 +47,10 @@ public class ProductQuantityPage {
         return this;
     }
 
-    @Step("Click to Verify Product Added to Cart with exact quantity")
+    @Step("Click on view Cart button")
     public ProductQuantityPage clickOnCart() {
         driver.element().click(viewCartbtn_button);
+        driver.browser().refreshCurrentPage();
         return this;
     }
 
@@ -54,9 +60,4 @@ public class ProductQuantityPage {
         return this;
     }
 
-    @Step("Verify that Product details is opened")
-    public ProductQuantityPage verifyOnProductDetails() {
-        driver.element().assertThat(productDetails).exists().perform();
-        return this;
-    }
 }
