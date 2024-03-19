8e57d9f6f9ecdc3f9c27f5b056a9f08e5e5347e2
Mariam Elsayed Abduallah <mariam.abduallah@gizasystems.local>
2024-03-19 16:41:44 +0200

    Create RegisterWhileCheckoutPage

---
+++ src/main/java/com/gizasystems/automationexercise/pages/RegisterWhileCheckoutPage.java
@@ -0,0 +1,10 @@
+package com.gizasystems.automationexercise.pages;
+
+import com.shaft.driver.SHAFT;
+
+public class RegisterWhileCheckoutPage {
+    private SHAFT.GUI.WebDriver driver;
+    public RegisterWhileCheckoutPage(SHAFT.GUI.WebDriver driver){
+        this.driver=driver;
+    }
+}
