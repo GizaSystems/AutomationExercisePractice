431e2d4a306868e8bc213e4588f280a0bb1633fc
Mariam Elsayed Abduallah <mariam.abduallah@gizasystems.local>
2024-03-19 17:07:04 +0200

    Prepare Test Data .json file

--- src/main/java/com/gizasystems/automationexercise/pages/RegisterWhileCheckoutPage.java
+++ src/main/java/com/gizasystems/automationexercise/pages/RegisterWhileCheckoutPage.java
@@ -3,7 +3,11 @@ package com.gizasystems.automationexercise.pages;
 import com.shaft.driver.SHAFT;
 
 public class RegisterWhileCheckoutPage {
+    // Variables
     private SHAFT.GUI.WebDriver driver;
+    //Locators
+
+    // Constructor
     public RegisterWhileCheckoutPage(SHAFT.GUI.WebDriver driver){
         this.driver=driver;
     }
--- src/test/java/com/gizasystems/automationexercise/tests/register/RegisterWhileCheckoutTests.java
+++ src/test/java/com/gizasystems/automationexercise/tests/register/RegisterWhileCheckoutTests.java
@@ -3,20 +3,48 @@ package com.gizasystems.automationexercise.tests.register;
 import com.gizasystems.automationexercise.apis.Apis;
 import com.gizasystems.automationexercise.pages.HomePage;
 import com.shaft.driver.SHAFT;
+import io.qameta.allure.*;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
+@TmsLink("")
+@Epic("")
+@Feature("")
+@Story("")
 public class RegisterWhileCheckoutTests {
+    // Variables
     private SHAFT.GUI.WebDriver driver;
     private SHAFT.API api;
     private SHAFT.TestData.JSON testData;
     private String timeStamp;
 
-
+    // Test Cases
+    @Test(description = "Place order : Register user While Checkout")
+    @Description("Given I open Automation Exercise home," +
+            " When I navigate to Recommended Products," +
+            " And I add Product to Cart," +
+            " And I click on Cart button ," +
+            " And I proceed to checkout," +
+            " And I click on Register /Login button " +
+            ", And I Fill all data and Create account," +
+            " And I Verify Account is created, " +
+            "And I Verify ' Logged in as username' at top," +
+            " And I .Click 'Cart' button, " +
+            "And I Click 'Proceed To Checkout' button," +
+            " And I Verify Address Details and Review Your Order," +
+            " And I Enter description in comment text area and click 'Place Order' ," +
+            " And I Enter payment details: Name on Card, Card Number, CVC, Expiration date," +
+            " And I Click 'Pay and Confirm Order' button," +
+            " And I Verify success message 'Your order has been placed successfully!' ," +
+            " And I Click 'Delete Account' button," +
+            " And I Verify 'ACCOUNT DELETED!' " +
+            "And click 'Continue' button")
+    //////////////////// Configurations \\\\\\\\\\
     @BeforeClass
     public void beforeClass() {
-
+        testData = new SHAFT.TestData.JSON("RegisterWhileCheckoutData.json");
     }
 
     @BeforeMethod
@@ -26,7 +54,7 @@ public class RegisterWhileCheckoutTests {
         new HomePage(driver)
                 .navigate()
                 .validateOnVisibilityOfHomePage();
-         timeStamp = String.valueOf(System.currentTimeMillis());
+        timeStamp = String.valueOf(System.currentTimeMillis());
     }
 
     @AfterMethod
