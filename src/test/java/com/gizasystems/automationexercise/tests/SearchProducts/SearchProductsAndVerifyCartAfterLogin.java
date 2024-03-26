9279e5d3f7692b895b3d119cf519859c35cd083d
abdelrahmangamal1998 <abdelrahmangamal.h@gmail.com>
2024-03-26 04:17:52 +0200

    refactored classes and added @TMS notations

--- src/test/java/com/gizasystems/automationexercise/tests/SearchProducts/SearchProductsAndVerifyCartAfterLogin.java
+++ src/test/java/com/gizasystems/automationexercise/tests/SearchProducts/SearchProductsAndVerifyCartAfterLogin.java
@@ -6,6 +6,7 @@ import com.shaft.driver.SHAFT;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Story;
+import io.qameta.allure.TmsLink;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
@@ -15,11 +16,11 @@ import org.testng.annotations.Test;
 @Feature("Product Search and Cart Verification After Login")
 @Story("Search Products and Verify Cart After Login")
 public class SearchProductsAndVerifyCartAfterLogin {
-
+    // Variables
     private SHAFT.GUI.WebDriver driver;
     private SHAFT.TestData.JSON testData;
-
-
+    //Testcases
+    @TmsLink("55512478")
     @Test(description = "Search Products and Verify Cart After Login")
     public void SearchProductsAndVerifyCart() {
 
