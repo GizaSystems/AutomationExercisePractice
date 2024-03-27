0491c1e5fd70aa1ff125d18c7c44298dd263a836
Mariam Elsayed Abduallah <mariam.abduallah@gizasystems.local>
2024-03-27 14:06:21 +0300

    Enhance login and login with APIs

--- src/test/java/com/gizasystems/automationexercise/tests/login/LoginUserApisTests.java
+++ src/test/java/com/gizasystems/automationexercise/tests/login/LoginUserApisTests.java
@@ -1,11 +1,9 @@
 package com.gizasystems.automationexercise.tests.login;
+
 import com.gizasystems.automationexercise.apis.Apis;
 import com.gizasystems.automationexercise.apis.ApisAccountManagement;
 import com.shaft.driver.SHAFT;
-import io.qameta.allure.Description;
-import io.qameta.allure.Epic;
-import io.qameta.allure.Feature;
-import io.qameta.allure.Story;
+import io.qameta.allure.*;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -14,32 +12,35 @@ import org.testng.annotations.Test;
 @Feature("User Management")
 @Story("Login")
 public class LoginUserApisTests {
+    // Variables
     private SHAFT.API api;
     private SHAFT.TestData.JSON testData;
-    private String userEmail;
+    private String timeStamp;
 
+    // Test Cases
+    @TmsLink("55512292")
     @Test(description = "Login User Test - API - Time Stamp")
     @Description("Given that I am a registered user, When I click in Signup/Login button, And I entered valid email and valid password, And I clicked on Login button, Then I should be logged in successfully to the system")
     public void loginUserTestApi() {
         new ApisAccountManagement(api)
-                .createRegisterUserAccount(testData.getTestData("UserName"), userEmail, testData.getTestData("UserPassword"), testData.getTestData("UserFirstName"), testData.getTestData("UserLastName"))
+                .createRegisterUserAccount(testData.getTestData("UserName"), testData.getTestData("UserMail.ApiTimeStamp") + timeStamp + "@gizasystems.com", testData.getTestData("UserPassword"), testData.getTestData("UserFirstName"), testData.getTestData("UserLastName"))
                 .validateUserCreatedRegistered()
-                .logIntoUserAccount(userEmail, testData.getTestData("UserPassword"))
+                .logIntoUserAccount(testData.getTestData("UserMail.ApiTimeStamp") + timeStamp + "@gizasystems.com", testData.getTestData("UserPassword"))
                 .validateUserLoggedIn()
-                .deleteUserAccount(userEmail, testData.getTestData("UserPassword"))
+                .deleteUserAccount(testData.getTestData("UserMail.ApiTimeStamp") + timeStamp + "@gizasystems.com", testData.getTestData("UserPassword"))
                 .validateDeleteUser()
-                .validateUserNotFound(userEmail);
+                .validateUserNotFound(testData.getTestData("UserMail.ApiTimeStamp") + timeStamp + "@gizasystems.com");
     }
 
     //////////////////// Configurations \\\\\\\\\\\\\\\\\\\\
     @BeforeClass
     public void beforeClass() {
-        testData = new SHAFT.TestData.JSON("src/test/resources/testDataFiles/RegisterUser.json");
-        userEmail=testData.getTestData("UserMail") + System.currentTimeMillis()  + "@gizasystems.com";
+        testData = new SHAFT.TestData.JSON("src/test/resources/testDataFiles/LoginUser.json");
     }
 
     @BeforeMethod
     public void beforeMethod() {
+        timeStamp = String.valueOf(System.currentTimeMillis());
         api = new SHAFT.API(Apis.ApisBaseUrl);
     }
 
